2a8520c32d06ef3fc846a247c6d3157a5bdf908a
Lit <Lighting@users.noreply.github.com>
2015-08-30 00:41:53 +0300

    Update pbtheme.c

--- pbtheme.c
+++ pbtheme.c
@@ -30,8 +30,8 @@ void usage(char **argv)
 {
 	fprintf(stderr, "Usage: %s [OPTION] <theme.pbt> [<theme.cfg>]\n",argv[0]);
 	fprintf(stderr, "Pack/unpack config of PocketBook theme\n");
-	fprintf(stderr, "\t-p\t\tpack\n");
-	fprintf(stderr, "\t-u\t\tunpack\n");
+	fprintf(stderr, "\t-p\tpack\n");
+	fprintf(stderr, "\t-u\tunpack\n");
 	exit(1);
 }
 
